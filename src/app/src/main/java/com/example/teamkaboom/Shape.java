087ee00cb4435e0d0b808e3112438fca572cef88
Anna Kibria <annakibria@gmail.com>
2026-01-27 18:42:00 -0700

    blue colour

--- src/app/src/main/java/com/example/teamkaboom/Shape.java
+++ src/app/src/main/java/com/example/teamkaboom/Shape.java
@@ -3,6 +3,7 @@ package com.example.teamkaboom;
 public abstract class Shape {
     private int x;
     private int y;
+    String color = "blue";
     public Shape(){
         x = 0;
         y = 0;
