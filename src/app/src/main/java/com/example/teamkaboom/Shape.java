27acb9bab6bdd257170ac4501edbf75ffd6b760c
Kevin Cao <kcao36@outlook.com>
2026-01-27 18:37:22 -0700

    Add colour property to Shape class

--- src/app/src/main/java/com/example/teamkaboom/Shape.java
+++ src/app/src/main/java/com/example/teamkaboom/Shape.java
@@ -3,6 +3,7 @@ package com.example.teamkaboom;
 public abstract class Shape {
     private int x;
     private int y;
+    private String colour = "purple";
     public Shape(){
         x = 0;
         y = 0;
